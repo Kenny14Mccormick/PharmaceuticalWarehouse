4f9b9c8527af3c53f6d2edddfc2c82201eba735e
Степан Хортов <khortov05@mail.ru>
2024-04-03 19:30:02 +0500

    Сделал страницу настроек

--- FolderPharmacyManager/Pages/UserSettings.xaml.cs
+++ FolderPharmacyManager/Pages/UserSettings.xaml.cs
@@ -22,21 +22,35 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
     /// </summary>
     public partial class UserSettings : Page, INotifyPropertyChanged
     {
-        private User _currentUser;
+
+        public User _currentUser;
         public User CurrentUser
         {
             get { return _currentUser; }
             set
             {
-                _currentUser = value;
-                OnPropertyChanged();
+                if (_currentUser != value)
+                {
+                    _currentUser = value;
+                    OnPropertyChanged();
+                }
             }
         }
+        private string _originalTitle;
+        private string _originalLogin;
+        private string _originalPassword;
+
         public UserSettings(User currentUser)
         {
+            _currentUser = currentUser;
             InitializeComponent();
-            CurrentUser = currentUser;
-            DataContext = this; // Устанавливаем текущий объект как контекст данных для окна
+
+            // Сохраняем оригинальные значения
+            _originalTitle = currentUser.Title;
+            _originalLogin = currentUser.Login;
+            _originalPassword = currentUser.Password;
+
+            DataContext = this;
         }
 
         // Реализация интерфейса INotifyPropertyChanged
@@ -50,6 +64,7 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
         // Обработчики событий для кнопок изменения и сохранения ника
         private void ChangeTitleButton_Click(object sender, RoutedEventArgs e)
         {
+            _originalTitle = _currentUser.Title;
             // Показываем TextBox для ввода нового ника
             tbTitle.Visibility = Visibility.Visible;
             tblTitle.Visibility = Visibility.Collapsed;
@@ -57,6 +72,8 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Сохранить", скрываем кнопку "Изменить"
             ChangeTitleButton.Visibility = Visibility.Collapsed;
             SaveTitleButton.Visibility = Visibility.Visible;
+            CancelUpdatingTitleButton.Visibility = Visibility.Visible;
+
         }
 
         private void SaveTitleButton_Click(object sender, RoutedEventArgs e)
@@ -66,12 +83,21 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             tblTitle.Visibility = Visibility.Visible;
 
             // Обновляем ник пользователя из TextBox
-            CurrentUser.Title = tbTitle.Text;
+            _currentUser.Title = tbTitle.Text;
 
             // Сохраняем изменения в базе данных
             try
             {
-                MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+                // Обновляем объект пользователя в базе данных
+                var userToUpdate = MainWindow.Pharmaceutical_Warehouse.User.FirstOrDefault(u => u.UserCode == _currentUser.UserCode);
+                if (userToUpdate != null)
+                {
+                    userToUpdate.Title = _currentUser.Title;
+                    MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+                }
+
+                // Сообщение об успешном сохранении
+                MessageBox.Show("Данные успешно сохранены.", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             catch (Exception ex)
             {
@@ -82,11 +108,14 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
             ChangeTitleButton.Visibility = Visibility.Visible;
             SaveTitleButton.Visibility = Visibility.Collapsed;
+            CancelUpdatingTitleButton.Visibility = Visibility.Collapsed;
         }
+
 
         // Обработчики событий для кнопок изменения и сохранения логина
         private void ChangeLoginButton_Click(object sender, RoutedEventArgs e)
         {
+            _originalLogin = _currentUser.Login;
             // Показываем TextBox для ввода нового логина
             tbLogin.Visibility = Visibility.Visible;
             tblLogin.Visibility = Visibility.Collapsed;
@@ -94,6 +123,8 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Сохранить", скрываем кнопку "Изменить"
             ChangeLoginButton.Visibility = Visibility.Collapsed;
             SaveLoginButton.Visibility = Visibility.Visible;
+            CancelUpdatingLoginButton.Visibility = Visibility.Visible;
+
         }
 
         private void SaveLoginButton_Click(object sender, RoutedEventArgs e)
@@ -103,12 +134,21 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             tblLogin.Visibility = Visibility.Visible;
 
             // Обновляем логин пользователя из TextBox
-            CurrentUser.Login = tbLogin.Text;
+            _currentUser.Login = tbLogin.Text;
 
             // Сохраняем изменения в базе данных
             try
             {
-                MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+                // Обновляем объект пользователя в базе данных
+                var userToUpdate = MainWindow.Pharmaceutical_Warehouse.User.FirstOrDefault(u => u.UserCode == _currentUser.UserCode);
+                if (userToUpdate != null)
+                {
+                    userToUpdate.Login = _currentUser.Login;
+                    MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+                }
+
+                // Сообщение об успешном сохранении
+                MessageBox.Show("Данные успешно сохранены.", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             catch (Exception ex)
             {
@@ -119,12 +159,14 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
             ChangeLoginButton.Visibility = Visibility.Visible;
             SaveLoginButton.Visibility = Visibility.Collapsed;
-
+            CancelUpdatingLoginButton.Visibility = Visibility.Collapsed;
         }
+
 
         // Обработчики событий для кнопок изменения и сохранения пароля
         private void ChangePasswordButton_Click(object sender, RoutedEventArgs e)
         {
+            _originalPassword = _currentUser.Password;
             // Показываем TextBox для ввода нового пароля
             tbPassword.Visibility = Visibility.Visible;
             tblPassword.Visibility = Visibility.Collapsed;
@@ -132,6 +174,7 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Сохранить", скрываем кнопку "Изменить"
             ChangePasswordButton.Visibility = Visibility.Collapsed;
             SavePasswordButton.Visibility = Visibility.Visible;
+            CancelUpdatingPasswordButton.Visibility = Visibility.Visible;
         }
 
         private void SavePasswordButton_Click(object sender, RoutedEventArgs e)
@@ -141,12 +184,22 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             tblPassword.Visibility = Visibility.Visible;
 
             // Обновляем пароль пользователя из TextBox
-            CurrentUser.Password = tbPassword.Text;
+            _currentUser.Password = tbPassword.Text;
 
             // Сохраняем изменения в базе данных
             try
             {
-                MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+
+                // Обновляем объект пользователя в базе данных
+                var userToUpdate = MainWindow.Pharmaceutical_Warehouse.User.FirstOrDefault(u => u.UserCode == _currentUser.UserCode);
+                if (userToUpdate != null)
+                {
+                    userToUpdate.Password = _currentUser.Password;
+                    MainWindow.Pharmaceutical_Warehouse.SaveChanges();
+                }
+
+                // Сообщение об успешном сохранении
+                MessageBox.Show("Данные успешно сохранены.", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             catch (Exception ex)
             {
@@ -157,7 +210,59 @@ namespace Аптечный_склад.FolderPharmacyManager.Pages
             // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
             ChangePasswordButton.Visibility = Visibility.Visible;
             SavePasswordButton.Visibility = Visibility.Collapsed;
+            CancelUpdatingPasswordButton.Visibility = Visibility.Collapsed;
 
+        }
+
+        private void CancelUpdatingPasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Возвращаем оригинальное значение логина
+            _currentUser.Password = _originalPassword;
+
+            // Скрыть TextBox для ввода нового логина и показать TextBlock с текущим логином
+            tbPassword.Visibility = Visibility.Collapsed;
+            tblPassword.Visibility = Visibility.Visible;
+
+            // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
+            ChangePasswordButton.Visibility = Visibility.Visible;
+            SavePasswordButton.Visibility = Visibility.Collapsed;
+            CancelUpdatingPasswordButton.Visibility = Visibility.Collapsed;
+
+            OnPropertyChanged(nameof(_currentUser));
+
+        }
+
+        private void CancelUpdatingLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Возвращаем оригинальное значение логина
+            _currentUser.Login = _originalLogin;
+
+            // Скрыть TextBox для ввода нового логина и показать TextBlock с текущим логином
+            tbLogin.Visibility = Visibility.Collapsed;
+            tblLogin.Visibility = Visibility.Visible;
+
+            // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
+            ChangeLoginButton.Visibility = Visibility.Visible;
+            SaveLoginButton.Visibility = Visibility.Collapsed;
+            CancelUpdatingLoginButton.Visibility = Visibility.Collapsed;
+            OnPropertyChanged(nameof(_currentUser));
+        }
+
+
+        private void CancelUpdatingTitleButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Возвращаем оригинальное значение логина
+            _currentUser.Title = _originalTitle;
+
+            // Скрыть TextBox для ввода нового логина и показать TextBlock с текущим логином
+            tbTitle.Visibility = Visibility.Collapsed;
+            tblTitle.Visibility = Visibility.Visible;
+
+            // Показываем кнопку "Изменить", скрываем кнопку "Сохранить"
+            ChangeTitleButton.Visibility = Visibility.Visible;
+            SaveTitleButton.Visibility = Visibility.Collapsed;
+            CancelUpdatingTitleButton.Visibility = Visibility.Collapsed;
+            OnPropertyChanged(nameof(_currentUser));
         }
     }
 }
